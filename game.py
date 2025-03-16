4d623e5a0f26aadb760a02f7b8808617bb204f9b
Pranav <dixitpranav24@gmail.com>
2025-03-16 21:23:54 +0530

    commit

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-# Python Program to illustrate
-# Hangman Game
 import random
 from collections import Counter
 
@@ -8,25 +6,23 @@ orange grape pineapple apricot lemon coconut watermelon
 cherry papaya berry peach lychee muskmelon'''
 
 someWords = someWords.split(' ')
-# randomly choose a secret word from our "someWords" LIST.
 word = random.choice(someWords)
 
 if __name__ == '__main__':
     print('Guess the word! HINT: word is a name of a fruit')
 
     for i in word:
-        # For printing the empty spaces for letters of the word
+
         print('_', end=' ')
     print()
 
     playing = True
-    # List for storing the letters guessed by the player
     letterGuessed = ''
     chances = len(word) + 2
     correct = 0
     flag = 0
     try:
-        while (chances != 0) and flag == 0:  # Flag is updated when the word is correctly guessed
+        while (chances != 0) and flag == 0: 
             print()
             chances -= 1
 
@@ -36,7 +32,6 @@ if __name__ == '__main__':
                 print('Enter only a letter!')
                 continue
 
-            # Validation of the guess
             if not guess.isalpha():
                 print('Enter only a LETTER')
                 continue
@@ -47,32 +42,27 @@ if __name__ == '__main__':
                 print('You have already guessed that letter')
                 continue
 
-            # If letter is guessed correctly
             if guess in word:
-                # k stores the number of times the guessed letter occurs in the word
                 k = word.count(guess)
                 for _ in range(k):
-                    letterGuessed += guess  # The guessed letter is added as many times as it occurs
+                    letterGuessed += guess  
 
-            # Print the word
             for char in word:
                 if char in letterGuessed and (Counter(letterGuessed) != Counter(word)):
                     print(char, end=' ')
                     correct += 1
-                # If user has guessed all the letters
-                # Once the correct word is guessed fully,
+                
                 elif (Counter(letterGuessed) == Counter(word)):
-                    # the game ends, even if chances remain
                     print("The word is: ", end=' ')
                     print(word)
                     flag = 1
                     print('Congratulations, You won!')
-                    break  # To break out of the for loop
-                    break  # To break out of the while loop
+                    break  
+                    break  
                 else:
                     print('_', end=' ')
 
-        # If user has used all of his chances
+
         if chances <= 0 and (Counter(letterGuessed) != Counter(word)):
             print()
             print('You lost! Try again..')
